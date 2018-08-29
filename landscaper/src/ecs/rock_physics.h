f5163309bbd6fa8c6a7568c9d186d3484cc9722f
llGuy <luc.rosenzweig@yahoo.com>
2018-08-29 09:19:42 +0100

    rocks sink before disappearing

--- landscaper/src/ecs/rock_physics.h
+++ landscaper/src/ecs/rock_physics.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ecs.h"
+#include "../timer.h"
 #include "../platform_handler.h"
 
 template <> class component <struct rock_physics> : public icomponent
@@ -8,6 +9,7 @@ template <> class component <struct rock_physics> : public icomponent
 private:
 	static constexpr f32 gravity_at_sea = -37.5f;
 	platform_handler * platforms;
+	bool in_water {false};
 public:
 	component(entity & subject, i32 index, platform_handler & ph)
 		: platforms(&ph), icomponent::icomponent(index)
@@ -20,25 +22,47 @@ public:
 		using detail::fequ;
 
 		auto & data = entities[entity_index].get_data();
-		data.pos += data.vel * td;
 
-		f32 ground_height = platforms->get_ground_height(data.pos.x, data.pos.z);
-
-		data.vel.y = data.vel.y + gravity_at_sea * td;
-
-		if (fequ(ground_height, data.pos.y) || ground_height > data.pos.y || fequ(data.vel.y, 0.0f))
+		if (in_water) sink(td, data, entities, ecs);
+		else
 		{
-			auto normal = platforms->get_normal(data.pos.x, data.pos.z);
-			/* the rock is outside of the platform */
-			if (normal.has_value()) data.vel = glm::reflect(data.vel, normal.value());
-		}
+			data.pos += data.vel * td;
+			data.vel.y = data.vel.y + gravity_at_sea * td;
 
-		/* kill rock if under ocean */
-		if (data.pos.y < -10.0f)
-		{
-			i32 index = entity_index;
-			ecs.remove(entities[index]);
-			entities.remove(index);
+			f32 ground_height = platforms->get_ground_height(data.pos.x, data.pos.z);
+
+			if (fequ(ground_height, data.pos.y) || ground_height > data.pos.y || fequ(data.vel.y, 0.0f))
+			{
+				/* landed in water */
+				if (fequ(ground_height, 0.0f))
+				{
+					data.vel = glm::vec3(0);
+					in_water = true;
+				}
+				/* still on terrain */
+				else bounce(td, data, ground_height);
+			}
 		}
+	}
+private:
+	auto kill_rock(vec_dd<entity> & entities, entity_cs & ecs) -> void
+	{
+		i32 index = entity_index;
+		ecs.remove(entities[index]);
+		entities.remove(index);
+	}
+	auto bounce(f32 td, entity_data & data, f32 ground_height) -> void
+	{
+		auto normal = platforms->get_normal(data.pos.x, data.pos.z);
+		/* the rock is outside of the platform */
+		if (normal.has_value()) data.vel = glm::reflect(data.vel, normal.value());
+		data.vel *= (0.7 - td / 2.0f);
+		data.pos.y = ground_height + 0.01f;
+	}
+	auto sink(f32 td, entity_data & data, vec_dd<entity> & entities, entity_cs & ecs) -> void
+	{
+		data.pos.y -= td / 10.0f;
+		/* kill rock */
+		if (data.pos.y < -3.0f) kill_rock(entities, ecs);
 	}
 };
